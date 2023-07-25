ec22df5a3f2b7e427dd5a00d151869970e442bf4
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-25 15:22:38 -0500

    Import the styles module to the NavBar component

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,12 +1,14 @@
+import styles from '../styles/navBar.module.css';
+
 export default function NavBar() {
   return (
     <header>
-      <nav>
-        <div className="logoContainer">
+      <nav className={styles.navBar}>
+        <div className={styles.logoContainer}>
           <img src="./planet.png" alt="logo" />
           <h1>Space Traveler's Hub</h1>
         </div>
-        <ul className="navigation">
+        <ul className={styles.navigation}>
           <li className="navLinks">Rockets</li>
           <li className="navLinks">Missions</li>
           <li className="navLinks">My Profile</li>
