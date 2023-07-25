5f862600e7bf9ff13797ba870012a35896773ea3
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-25 15:35:25 -0500

    Apply the navLinks styles class

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -6,12 +6,12 @@ export default function NavBar() {
       <nav className={styles.navBar}>
         <div className={styles.logoContainer}>
           <img src="./planet.png" alt="logo" />
-          <h1>Space Traveler's Hub</h1>
+          <h1>Space Traveler&apos;s Hub</h1>
         </div>
         <ul className={styles.navigation}>
-          <li className="navLinks">Rockets</li>
-          <li className="navLinks">Missions</li>
-          <li className="navLinks">My Profile</li>
+          <li className={styles.navLinks}>Rockets</li>
+          <li className={styles.navLinks}>Missions</li>
+          <li className={styles.navLinks}>My Profile</li>
         </ul>
       </nav>
     </header>
