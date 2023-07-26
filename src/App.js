ce9489a82f4ed772822c659574fb9880d7ae8747
Braingenius2 <braingenius2@gmail.com>
2023-07-26 09:54:07 +0300

    Add routes and routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import NavBar from './components/navBar';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/navBar';
+import Rockets from './components/rockets';
+import Missions from './components/missions';
+import MyProfile from './components/myProfile';
+import Dragons from './components/dragons';
 
 function App() {
   return (
-    <NavBar />
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route index element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+          <Route path="/profile" element={<MyProfile />} />
+          <Route path="/dragons" element={<Dragons />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
