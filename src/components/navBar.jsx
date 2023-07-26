6dc2b3847b3a0643d2763054620f900ce6cc7ed9
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-25 20:16:07 -0500

    Modify the useEffect hook

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,6 +1,6 @@
-import styles from '../styles/navBar.module.css';
 import { NavLink, useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import styles from '../styles/navBar.module.css';
 
 export default function NavBar() {
   const location = useLocation();
@@ -10,7 +10,7 @@ export default function NavBar() {
     // Get the actual path without the slash.
     const currentPath = location.pathname.substring(1);
     setCurrentPage(currentPath);
-  });
+  }, [location]);
   return (
     <header>
       <nav className={styles.navBar}>
@@ -22,7 +22,7 @@ export default function NavBar() {
           <li className={styles.navLinks}>Rockets</li>
           <li className={`${styles.navLinks} ${currentPage === 'missions' ? styles.active : ''}`}>
             <NavLink to="missions">Missions</NavLink>
-            </li>
+          </li>
           <li className={styles.navLinks}>My Profile</li>
         </ul>
       </nav>
