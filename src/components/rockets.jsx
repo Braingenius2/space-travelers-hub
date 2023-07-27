b5bd66fc69d2d35f0c9d1f351d1ad78f1d5fb729
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-27 15:38:55 -0500

    Add a basic structure to render the Rockets

--- src/components/rockets.jsx
+++ src/components/rockets.jsx
@@ -1,7 +1,18 @@
-const Rockets = () => (
-  <div>
-    <h2>Rockets</h2>
-  </div>
-);
+import { useSelector } from 'react-redux';
+
+const Rockets = () => {
+  const rocketsList = useSelector((state) => state.rockets);
+  return (
+    <>
+      {rocketsList.rockets.map((rocket) => (
+        <div key={rocket.id}>
+          <img src={rocket.flickr_images[0]} alt={rocket.name} />
+          <p>{rocket.rocket_name}</p>
+          <p>{rocket.rocket_type}</p>
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default Rockets;
