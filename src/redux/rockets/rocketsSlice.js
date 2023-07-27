47c570da8e3f30f503fca28597c73bec5074dc75
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-27 15:04:58 -0500

    Create the slice for the rockets

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,4 +1,4 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const API = 'https://api.spacexdata.com/v3/rockets';
@@ -11,3 +11,31 @@ const getRockets = createAsyncThunk('rockets/getRockets', async () => {
     throw new Error("Couldn't get Rockets data");
   }
 });
+
+const initialState = {
+  rockets: [],
+  status: 'idle',
+  error: '',
+};
+
+const RocketsSlice = createSlice({
+  name: 'rockets',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(getRockets.fulfilled, (state, action) => {
+        state.status = 'succeed';
+        state.rockets = action.payload;
+      })
+      .addCase(getRockets.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getRockets.rejected, (state, action) => {
+        state.status = 'rejected';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default RocketsSlice.reducer;
