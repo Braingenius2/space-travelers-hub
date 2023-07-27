00ddd6f021ae1987db44d854ce3f653645f60ad7
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-27 15:38:24 -0500

    Dispatch the getRockets function to the store

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
+import { getRockets } from './redux/rockets/rocketsSlice';
+
+store.dispatch(getRockets());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
