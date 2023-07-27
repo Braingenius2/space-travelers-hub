02ed125d4ac585413a0eb01440707b9ddebe60e1
Braingenius2 <braingenius2@gmail.com>
2023-07-27 16:43:08 +0300

    fetch missions data

--- src/components/missions.jsx
+++ src/components/missions.jsx
@@ -1,7 +1,29 @@
-const Missions = () => (
-  <div>
-    <h2>Missions</h2>
-  </div>
-);
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchMissions } from "../redux/missions/missionsSlice";
+
+const Missions = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions.missions);
+
+  useEffect(() => {
+    dispatch(fetchMissions);
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h2>Missions</h2>
+      <div>
+        {missions.map((mission) => (
+          <div>
+            <div>{mission.mission_id}</div>
+            <div>{mission.mission_name}</div>
+            <div>{mission.description}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Missions;
