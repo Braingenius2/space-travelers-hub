c72122ad821280464847ff58bc88074b22cb2e9c
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-28 14:28:37 -0500

    Implemente the reserveDragon action and modify the elements dependings on the reserved property

--- src/components/dragons.jsx
+++ src/components/dragons.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchDragons } from '../redux/dragons/dragonsSlice';
+import { fetchDragons, reserveDragon } from '../redux/dragons/dragonsSlice';
 import styles from '../styles/dragons.module.css';
 
 const Dragons = () => {
@@ -19,10 +19,18 @@ const Dragons = () => {
           <div>
             <h2>{dragons.name}</h2>
             <p>
-              <span className={styles.reserveSpan}>Reserved</span>
+              {dragons.reserved && <span className={styles.reserveSpan}>Reserved</span>}
               {dragons.description}
             </p>
-            <button type="button" className={styles.reserveButton}>Reserve Dragon</button>
+            <button
+              type="button"
+              className={dragons.reserved ? styles.cancelButton : styles.reserveButton}
+              onClick={() => {
+                dispatch(reserveDragon(dragons.id));
+              }}
+            >
+              {dragons.reserved ? 'Cancel Reservation' : 'Reserve Dragon'}
+            </button>
           </div>
         </div>
       ))}
