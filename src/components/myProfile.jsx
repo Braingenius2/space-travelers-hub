0bbd3e877c00e6c6e1c9d910c7bb358cd6455bc1
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-28 17:13:47 -0500

    Created a dragonsList function to render the reserved dragons

--- src/components/myProfile.jsx
+++ src/components/myProfile.jsx
@@ -1,5 +1,40 @@
-const MyProfile = () => (
-  <div />
-);
+import { useSelector } from 'react-redux/es/hooks/useSelector';
+
+const MyProfile = () => {
+  const { dragons } = useSelector((state) => state.dragons);
+
+  function dragonsList() {
+    const reservedDragons = dragons.filter((dragon) => dragon.reserved === true);
+
+    if (reservedDragons.length) {
+      return reservedDragons.map((dragon) => (
+        <tr key={dragon.id}>
+          <td>{dragon.name}</td>
+        </tr>
+      ));
+    }
+
+    return (
+      <tr>
+        <td>
+          <i>No Dragons</i>
+        </td>
+      </tr>
+    );
+  }
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>My Dragons</th>
+          </tr>
+        </thead>
+        <tbody>{dragonsList()}</tbody>
+      </table>
+    </div>
+  );
+};
 
 export default MyProfile;
