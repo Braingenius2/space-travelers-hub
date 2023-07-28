b9a41ab5c636d7e9978abe65575f6d3e96697816
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-28 14:10:35 -0500

    Import styles to the dragons component

--- src/components/dragons.jsx
+++ src/components/dragons.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchDragons } from '../redux/dragons/dragonsSlice';
+import styles from '../styles/dragons.module.css';
 
 const Dragons = () => {
   const dispatch = useDispatch();
@@ -11,22 +12,20 @@ const Dragons = () => {
   }, [dispatch]);
 
   return (
-    <div>
-      <div>
-        {dragons.map((dragons) => (
-          <div key={dragons.id} className="dragons">
-            <img src={dragons.flickr_images[0]} alt={dragons.name} />
-            <div>
-              <h2>{dragons.name}</h2>
-              <p>
-                <span className="reserveSpan" />
-                {dragons.description}
-              </p>
-              <button type="button" className="reserveButton">Reserve Dragon</button>
-            </div>
+    <div className={styles.container}>
+      {dragons.map((dragons) => (
+        <div key={dragons.id} className={styles.dragons}>
+          <img src={dragons.flickr_images[0]} alt={dragons.name} className={styles.images} />
+          <div>
+            <h2>{dragons.name}</h2>
+            <p>
+              <span className={styles.reserveSpan}>Reserved</span>
+              {dragons.description}
+            </p>
+            <button type="button" className={styles.reserveButton}>Reserve Dragon</button>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 };
