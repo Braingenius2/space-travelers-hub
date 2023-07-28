f05ca10497eb7661a2a3a3b8315b712126696bac
Braingenius2 <braingenius2@gmail.com>
2023-07-28 12:21:32 +0300

    Add styles to missions component

--- src/components/missions.jsx
+++ src/components/missions.jsx
@@ -2,10 +2,17 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Table from 'react-bootstrap/Table';
 import { fetchMissions } from '../redux/missions/missionsSlice';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Missions = () => {
   const dispatch = useDispatch();
   const { missions, isLoading, error } = useSelector((state) => state.missions);
+
+  const style = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  };
 
   useEffect(() => {
     dispatch(fetchMissions());
@@ -15,25 +22,28 @@ const Missions = () => {
   if (error) return (<div>{error}</div>);
 
   return (
-    <Table striped bordered hover responsive variant="dark">
-      <thead>
-        <tr>
-          <th>Mission</th>
-          <th>Descripton</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {missions.map((mission) => (
-          <tr key={mission.mission_id}>
-            <td>{mission.mission_name}</td>
-            <td>{mission.description}</td>
-            <td>NOT A MEMBER</td>
-            <td><button type="button">Join Misson</button></td>
+    <div style={style}>
+      <Table striped bordered hover style={{ width: '90%' }}>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Descripton</th>
+            <th>Status</th>
+            <th><p style={{ display: 'none' }}>Action</p></th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {missions.map((mission) => (
+            <tr key={mission.mission_id}>
+              <td>{mission.mission_name}</td>
+              <td>{mission.description}</td>
+              <td>NOT A MEMBER</td>
+              <td><button type="button" aria-label="Join Mission">Join Misson</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   );
 };
 
