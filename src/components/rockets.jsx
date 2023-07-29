24b402bff83d9c1591c70d3cbb15d7261cbd22a4
Braingenius2 <braingenius2@gmail.com>
2023-07-29 08:01:44 +0300

    Refactor display of rockets

--- src/components/rockets.jsx
+++ src/components/rockets.jsx
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { reserveRocket, cancelReservation } from '../redux/rockets/rocketsSlice';
+import styles from '../styles/dragons.module.css';
 
 const Rockets = () => {
   const dispatch = useDispatch();
@@ -14,22 +15,28 @@ const Rockets = () => {
   };
 
   return (
-    <>
+    <div className={styles.container}>
       {rockets.map((rocket) => (
-        <div key={rocket.id}>
-          <img src={rocket.flickr_images[0]} alt={rocket.name} />
-          <p>{rocket.rocket_name}</p>
-          <p>{rocket.rocket_type}</p>
-          {/* Render the reservation button or cancel reservation button */}
-          {/* based on the reserved status */}
-          {rocket.reserved ? (
-            <button type="button" onClick={() => handleCancelReservation(rocket.id)}>Cancel Reservation</button>
-          ) : (
-            <button type="button" onClick={() => handleReserveRocket(rocket.id)}>Reserve Rocket</button>
-          )}
+        <div key={rocket.id} className={styles.dragons}>
+          <img src={rocket.flickr_images[0]} alt={rocket.name} className={styles.images} />
+          <div>
+            <h2>{rocket.rocket_name}</h2>
+            <p>
+              {rocket.reserved && <span className={styles.reserveSpan}>Reserved</span>}
+              {' '}
+              {rocket.description}
+            </p>
+            {/* Render the reservation button or cancel reservation button */}
+            {/* based on the reserved status */}
+            {rocket.reserved ? (
+              <button type="button" onClick={() => handleCancelReservation(rocket.id)} className={styles.cancelButton}>Cancel Reservation</button>
+            ) : (
+              <button type="button" onClick={() => handleReserveRocket(rocket.id)} className={styles.reserveButton}>Reserve Rocket</button>
+            )}
+          </div>
         </div>
       ))}
-    </>
+    </div>
   );
 };
 
