12f04b1d4090157dd518dd18e16e11480972eddc
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-29 11:22:37 -0500

    Created unit test for Rockets component

--- src/test/Rockets.test.js
+++ src/test/Rockets.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'; // For better DOM assertions
-import Rockets from '../components/Rockets';
+import Rockets from '../components/rockets';
 
 // Mocking useDispatch and useSelector
 jest.mock('react-redux', () => ({
